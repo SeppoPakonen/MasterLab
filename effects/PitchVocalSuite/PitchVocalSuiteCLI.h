986aa87081ffb233dd4bf1b47c23434103ac74ed
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-03-04 15:13:35 +0200

    Verify note-based pitch correction on real vocal audio

--- effects/PitchVocalSuite/PitchVocalSuiteCLI.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PitchVocalSuite_PitchVocalSuiteCLI_h_
-#define _PitchVocalSuite_PitchVocalSuiteCLI_h_
-
-#include <Core/Core.h>
-#include "PitchVocalSuite.h"
-
-using namespace Upp;
-
-#endif
