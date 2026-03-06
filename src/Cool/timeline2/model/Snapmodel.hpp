dcae451d9ef4c13297b9d719820e5b1aa71a8bd0
Seppo Pakonen <seppo.pakonen@gmail.com>
2026-03-06 21:47:19 +0200

    Cool: Genuine U++ conversion of Timeline, Track, Clip, Snap, and Group models.

--- src/Cool/timeline2/model/Snapmodel.hpp
+++ src/Cool/timeline2/model/Snapmodel.hpp
@@ -1,84 +1,43 @@
-// Converted from tmp/k/src/timeline2/model/snapmodel.hpp
-// Phase-1 mechanical conversion: framework-specific includes are commented for later U++ wiring.
-
 /*
     SPDX-FileCopyrightText: 2017 Nicolas Carion
-    SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+    U++ Conversion: 2026 MasterLab Team
 */
 
-#pragma once
+#ifndef _Cool_SnapModel_h_
+#define _Cool_SnapModel_h_
 
-// #include <map>
-// #include <vector>
+#include <Core/Core.h>
 
-/** @class SnapInterface
-    @brief This is a base class for snap models (timeline, clips)
-    Implements only basic functions like add or remove snap points
- */
-class SnapInterface
-{
+NAMESPACE_UPP
+
+class SnapInterface {
 public:
-    SnapInterface();
-    virtual ~SnapInterface();
-    /** @brief Adds a snappoint at given position */
-    virtual void addPoint(int position) = 0;
-
-    /** @brief Removes a snappoint from given position */
-    virtual void removePoint(int position) = 0;
+    virtual ~SnapInterface() {}
+    virtual void AddPoint(int position) = 0;
+    virtual void RemovePoint(int position) = 0;
 };
 
-/** @class SnapModel
-    @brief This class represents the snap points of the timeline.
-    Basically, one can add or remove snap points, and query the closest snap point to a given location
- */
-class SnapModel : public virtual SnapInterface
-{
+class SnapModel : public SnapInterface {
 public:
     SnapModel();
+    virtual ~SnapModel();
 
-    /** @brief Adds a snappoint at given position */
-    void addPoint(int position) override;
+    virtual void AddPoint(int position) override;
+    virtual void RemovePoint(int position) override;
 
-    /** @brief Removes a snappoint from given position */
-    void removePoint(int position) override;
+    int GetClosestPoint(int position) const;
+    int GetNextPoint(int position) const;
+    int GetPreviousPoint(int position) const;
 
-    /** @brief Retrieves closest point. Returns -1 if there is no snappoint available */
-    int getClosestPoint(int position);
-
-    /** @brief Retrieves next snap point. Returns position if there is no snappoint available */
-    int getNextPoint(int position);
-
-    /** @brief Retrieves previous snap point. Returns 0 if there is no snappoint available */
-    int getPreviousPoint(int position);
-
-    /** @brief Ignores the given positions until unIgnore() is called
-       You can make several call to this before unIgnoring
-       Note that you cannot remove ignored points.
-       @param points list of point to ignore
-     */
-    void ignore(const std::vector<int> &pts);
-
-    /** @brief Revert ignoring
-     */
-    void unIgnore();
-
-    /** @brief Propose a size for the item (clip, composition,...) being resized, based on the snap points.
-       @param in current inpoint of the item
-       @param out current outpoint of the item
-       @param size is the size requested before snapping
-       @param right true if we resize the right end of the item
-       @param maxSnapDist maximal number of frames we are allowed to snap to
-    */
-    int proposeSize(int in, int out, int size, bool right, int maxSnapDist);
-    int proposeSize(int in, int out, const std::vector<int> &boundaries, int size, bool right, int maxSnapDist);
-
-    // For testing only
-    std::map<int, int> _snaps() { return m_snaps; }
+    void Ignore(const Vector<int>& points);
+    void UnIgnore();
 
 private:
-    /** This represents the snappoints internally. The keys are the positions and the values are the number of elements at this
-     * position. Note that it is important that the datastructure is ordered. QMap is NOT ordered, and therefore not suitable.
-     */
-    std::map<int, int> m_snaps;
-    std::vector<int> m_ignore;
+    // Sorted list of snap points (position -> count)
+    VectorMap<int, int> snaps;
+    Vector<int> ignored_points;
 };
+
+END_UPP_NAMESPACE
+
+#endif
